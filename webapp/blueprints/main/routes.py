9e445cabd554a72497d36c8461847b2867300145
Pierpaolo Calanna <p.calanna@gmail.com>
2020-01-19 20:00:17 +0100

    refactoring how lemma are listed (work in progress)

--- webapp/blueprints/main/routes.py
+++ webapp/blueprints/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, url_for, abort
+from flask import render_template, request, url_for, abort, redirect
 from flask_login import login_required 
 from webapp.database.sqlite import query_db
 import re
@@ -8,7 +8,23 @@ from . import bp_main
 # ################################################################################
 # GLOBAL CONSTANTS AND VARS
 # ################################################################################
-PER_PAGE = 10
+PER_PAGE = 4
+
+ # init data object
+data = {
+  "words_n_all"    : 0,
+  "words_n_filter" : 0,
+  "words_list"     : [], 
+  "first_rec"      : None,
+  "last_rec"       : None,
+  "prev_rec"       : None,
+  "next_rec"       : None,
+  "is_first_page"  : None,
+  "is_last_page"   : None,
+  "prev_url"       : None,
+  "next_url"       : None,
+}
+
 
 # ################################################################################
 # ROUTES 
@@ -20,79 +36,70 @@ PER_PAGE = 10
 @bp_main.route("/")
 @login_required
 def index():
+    # redirect
+    return redirect(url_for("main.list", offset_field = "lemma", offset_value = None, order_dir = "ASC"))
+
+@bp_main.route("/list/offset_field/<offset_field>/order_dir/<order_dir>")
+@login_required
+def list(offset_field, order_dir):
 
   # cache params
-  offset_field  = request.args.get("offset_field", "lemma")
-  offset_value  = request.args.get("offset_value", "0")
-  filter_by = request.args.get("filter_by", None)
-  order_dir  = request.args.get("order_dir", "ASC").upper()
+  offset_value = request.args.get("offset_value", None)
+  offset_dir = request.args.get("offset_dir", "next")
 
   # sanity checks
-  if offset_field and offset_field not in ("lemma", "visited"): abort(400)
+  if offset_field not in ("lemma","visited"): abort(400)
   if offset_value and not re.sub("\s|'|\-","", offset_value).isalnum(): abort(400)
-  if order_dir and order_dir not in ("ASC", "DESC"): abort(400)
-  if filter_by:
-    for letter in filter_by:
-      if letter.upper() not in "ABCDEFGHIJKLMOPQRSTUVWXYZ '-": abort(400)
+  if offset_dir not in ("prev", "next"): abort(400)
+  if order_dir not in ("ASC", "DESC"): abort(400)
 
-  # init data object
-  data = {
-    "words_n_all"    : 0,
-    "words_n_filter" : 0,
-    "words_list"     : [],
-    "first_rec"      : None,
-    "last_rec"       : None,
-    "prev_rec"       : None,
-    "next_rec"       : None,
-    "is_first_page"  : None,
-    "is_last_page"   : None
-  }
+  # set operator base on offset_dir and order_dir
+  if order_dir == "ASC":
+    operator = ">" if offset_dir == "next" else "<" 
+    if not offset_value: offset_value = "0"
 
-  # set operator base on direction
-  operator = ">" if order_dir == "ASC" else "<"
+  if order_dir == "DESC": 
+    operator = "<" if offset_dir == "next" else ">"
+    if not offset_value: offset_value = "zz"
 
-  # fixed sql strings with parameters
+  # fixed sql queries with parameters
   sql = [
-    (f"SELECT COUNT(*) as c FROM lemmi", [ None ]),
-    (f"SELECT COUNT(*) as c FROM lemmi", [ None ]),
-    (f"SELECT ROWID,* FROM lemmi WHERE {offset_field} {operator} ? ORDER BY {offset_field} {order_dir} LIMIT ?", [ offset_value, PER_PAGE ]),
-    (f"SELECT * FROM lemmi ORDER BY {offset_field} ASC LIMIT 1", [ None ]),
-    (f"SELECT * FROM lemmi ORDER BY {offset_field} DESC LIMIT 1", [ None]),
-    (f"SELECT COUNT(*) as c FROM lemmi WHERE lemma LIKE ?", [ f"{filter_by}%" ]),
-    (f"SELECT ROWID,* FROM lemmi WHERE {offset_field} {operator} ? AND lemma LIKE ? ORDER BY {offset_field} {order_dir} LIMIT ?", \
-        [ offset_value, f"{filter_by}%", PER_PAGE]),
-    (f"SELECT * FROM lemmi WHERE lemma LIKE ? ORDER BY {offset_field} ASC LIMIT 1", [ f"{filter_by}%" ]),
-    (f"SELECT * FROM lemmi WHERE lemma LIKE ? ORDER BY {offset_field} DESC LIMIT 1",[ f"{filter_by}%" ])
-  ]
-  
-  # pop out unused sql
-  if filter_by: 
-      del sql[1:5] 
-  else: 
-      del sql[5:]
- 
+        (f"SELECT COUNT(*) as c FROM lemmi",),
+        (f"SELECT * FROM lemmi ORDER BY {offset_field} ASC LIMIT 1",),
+        (f"SELECT * FROM lemmi ORDER BY {offset_field} DESC LIMIT 1",),
+        (f"SELECT ROWID,* FROM lemmi WHERE {offset_field} {operator} ? ORDER BY {offset_field} ASC LIMIT ?", [ offset_value, PER_PAGE ]),
+        (f"SELECT ROWID,* FROM lemmi WHERE {offset_field} {operator} ? ORDER BY {offset_field} DESC LIMIT ?", [ offset_value, PER_PAGE ]),
+    ]
+
   # fetch data from db
-  data["words_n_all"]    = int(query_db(sql[0][0], [ p for p in sql[0][1] if p ],  one = True)["c"]) 
-  data["words_n_filter"] = int(query_db(sql[1][0], [ p for p in sql[1][1] if p ], one = True)["c"])
-  data["words_list"]     = query_db(sql[2][0], [ p for p in sql[2][1] if p ])
+  data["words_n_all"]    = int(query_db(sql[0][0], one = True)["c"]) 
+  data["words_n_filter"] = data["words_n_all"]
   
-  # reverse words list if necessary
-  if order_dir == "DESC": data["words_list"] = data["words_list"][::-1]
+  if order_dir == "ASC" and offset_dir == "next": data["words_list"]  = query_db(sql[-2][0], sql[-1][1])
+  if order_dir == "ASC" and offset_dir == "prev": data["words_list"]  = query_db(sql[-1][0], sql[-1][1])
+  if order_dir == "DESC" and offset_dir == "next": data["words_list"] = query_db(sql[-1][0], sql[-1][1])
+  if order_dir == "DESC" and offset_dir == "prev": data["words_list"] = query_db(sql[-2][0], sql[-1][1])
+
+  if offset_dir == "prev":
+    data["words_list"] = data["words_list"][::-1]
 
   # if there are words
   if data["words_list"]:
-    
+     
     # add other pagination data
-    data["first_rec"] = query_db(sql[3][0], [ p for p in sql[3][1] if p ],one = True)[offset_field]
-    data["last_rec"]  = query_db(sql[4][0], [ p for p in sql[4][1] if p ], one = True)[offset_field]
-    data["prev_rec"]  = data["words_list"][0][offset_field]
-    data["next_rec"]  = data["words_list"][-1][offset_field]
+    if order_dir == "ASC":
+        data["first_rec"] = query_db(sql[1][0], one = True)[offset_field]
+        data["last_rec"]  = query_db(sql[2][0], one = True)[offset_field]
+    else:
+        data["first_rec"] = query_db(sql[2][0], one = True)[offset_field]
+        data["last_rec"]  = query_db(sql[1][0], one = True)[offset_field]
 
-  # add other pagination data
-  data["is_first_page"] = data["first_rec"] == data["prev_rec"]
-  data["is_last_page"]  = data["last_rec"] == data["next_rec"]
-  data["prev_url"]      = url_for("main.index", filter_by = filter_by, offset_field = offset_field, offset_value = data["prev_rec"], order_dir = "DESC")
-  data["next_url"]      = url_for("main.index", filter_by = filter_by, offset_field = offset_field, offset_value = data['next_rec'], order_dir = "ASC")
+    data["prev_rec"]      = data["words_list"][0][offset_field]
+    data["next_rec"]      = data["words_list"][-1][offset_field]
+    data["is_first_page"] = data["first_rec"] == data["prev_rec"]
+    data["is_last_page"]  = data["last_rec"] == data["next_rec"]
+    data["prev_url"]      = url_for("main.list", offset_field = offset_field, offset_value = data["prev_rec"], offset_dir = "prev", order_dir = order_dir)
+    data["next_url"]      = url_for("main.list", offset_field = offset_field, offset_value = data['next_rec'], offset_dir = "next", order_dir = order_dir)
   
   # render view
   return render_template("main/index.html", data = data)
