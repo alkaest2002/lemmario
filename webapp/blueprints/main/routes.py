3ade77a99ecc053cf1d127392c7abd9ad7ce028d
Pierpaolo Calanna <p.calanna@gmail.com>
2020-01-20 08:54:03 +0100

    refatored main view list

--- webapp/blueprints/main/routes.py
+++ webapp/blueprints/main/routes.py
@@ -21,8 +21,11 @@ data = {
   "next_rec"       : None,
   "is_first_page"  : None,
   "is_last_page"   : None,
-  "prev_url"       : None,
-  "next_url"       : None,
+  "offset_field"   : None,
+  "offset_dir"     : None,
+  "filter_field"   : None,
+  "filter_value"   : None,
+  "order_dir"      : None
 }
 
 
@@ -37,23 +40,32 @@ data = {
 @login_required
 def index():
     # redirect
-    return redirect(url_for("main.list", offset_field = "lemma", offset_value = None, order_dir = "ASC"))
+    return redirect(url_for("main.list_lemma", offset_field = "lemma", order_dir = "ASC"))
 
+# --------------------------------------------------------------------------------
+# list all words with offset and order
+# --------------------------------------------------------------------------------
 @bp_main.route("/list/offset_field/<offset_field>/order_dir/<order_dir>")
 @login_required
-def list(offset_field, order_dir):
+def list_lemma(offset_field, order_dir):
 
   # cache params
   offset_value = request.args.get("offset_value", None)
+  filter_field = request.args.get("filter_field", None)
+  filter_value = request.args.get("filter_value", None)
   offset_dir = request.args.get("offset_dir", "next")
 
   # sanity checks
-  if offset_field not in ("lemma","visited"): abort(400)
-  if offset_value and not re.sub("\s|'|\-","", offset_value).isalnum(): abort(400)
-  if offset_dir not in ("prev", "next"): abort(400)
-  if order_dir not in ("ASC", "DESC"): abort(400)
+  if any((
+    offset_field not in ("lemma","visited"),
+    offset_value and not re.sub("\s|'|\-","", offset_value).isalnum(),  
+    filter_field and filter_field not in ("lemma",),
+    filter_value and not re.sub("\s|'|\-","", filter_value).isalnum(),
+    offset_dir not in ("prev", "next"),
+    order_dir not in ("ASC", "DESC")
+  )): abort(400)
 
-  # set operator base on offset_dir and order_dir
+  # set operator and offset_value
   if order_dir == "ASC":
     operator = ">" if offset_dir == "next" else "<" 
     if not offset_value: offset_value = "0"
@@ -62,44 +74,64 @@ def list(offset_field, order_dir):
     operator = "<" if offset_dir == "next" else ">"
     if not offset_value: offset_value = "zz"
 
-  # fixed sql queries with parameters
-  sql = [
-        (f"SELECT COUNT(*) as c FROM lemmi",),
-        (f"SELECT * FROM lemmi ORDER BY {offset_field} ASC LIMIT 1",),
-        (f"SELECT * FROM lemmi ORDER BY {offset_field} DESC LIMIT 1",),
+  # cache fixed sql queries with parameters
+  sql_all = [
+    [
+        (f"SELECT COUNT(*) as c FROM lemmi", []),
+        (f"SELECT COUNT(*) as c FROM lemmi", []),
+        (f"SELECT * FROM lemmi ORDER BY {offset_field} ASC LIMIT 1", []),
+        (f"SELECT * FROM lemmi ORDER BY {offset_field} DESC LIMIT 1", []),
         (f"SELECT ROWID,* FROM lemmi WHERE {offset_field} {operator} ? ORDER BY {offset_field} ASC LIMIT ?", [ offset_value, PER_PAGE ]),
         (f"SELECT ROWID,* FROM lemmi WHERE {offset_field} {operator} ? ORDER BY {offset_field} DESC LIMIT ?", [ offset_value, PER_PAGE ]),
+    ],
+    [
+        (f"SELECT COUNT(*) as c FROM lemmi", []),
+        (f"SELECT COUNT(*) as c FROM lemmi WHERE {filter_field} LIKE ?", [ f"{filter_value}%" ]),
+        (f"SELECT * FROM lemmi WHERE {filter_field} LIKE ? ORDER BY {offset_field} ASC LIMIT 1", [ f"{filter_value}%" ]),
+        (f"SELECT * FROM lemmi WHERE {filter_field} LIKE ? ORDER BY {offset_field} DESC LIMIT 1", [ f"{filter_value}%" ]),
+        (f"SELECT ROWID,* FROM lemmi WHERE {filter_field} LIKE ? AND {offset_field} {operator} ? ORDER BY {offset_field} ASC LIMIT ?", [ f"{filter_value}%", offset_value, PER_PAGE ]),
+        (f"SELECT ROWID,* FROM lemmi WHERE {filter_field} LIKE ? AND {offset_field} {operator} ? ORDER BY {offset_field} DESC LIMIT ?", [ f"{filter_value}%", offset_value, PER_PAGE ]),
     ]
+  ]
+
+  # choose sql queeries
+  sql = sql_all[0] if not filter_value else sql_all[1]
 
   # fetch data from db
-  data["words_n_all"]    = int(query_db(sql[0][0], one = True)["c"]) 
-  data["words_n_filter"] = data["words_n_all"]
+  data["words_n_all"]    = int(query_db(sql[0][0], sql[0][1], one = True)["c"]) 
+  data["words_n_filter"] = int(query_db(sql[0][0], sql[0][1], one = True)["c"])
   
-  if order_dir == "ASC" and offset_dir == "next": data["words_list"]  = query_db(sql[-2][0], sql[-1][1])
+  if order_dir == "ASC" and offset_dir == "next": data["words_list"]  = query_db(sql[-2][0], sql[-2][1])
   if order_dir == "ASC" and offset_dir == "prev": data["words_list"]  = query_db(sql[-1][0], sql[-1][1])
   if order_dir == "DESC" and offset_dir == "next": data["words_list"] = query_db(sql[-1][0], sql[-1][1])
-  if order_dir == "DESC" and offset_dir == "prev": data["words_list"] = query_db(sql[-2][0], sql[-1][1])
+  if order_dir == "DESC" and offset_dir == "prev": data["words_list"] = query_db(sql[-2][0], sql[-2][1])
 
+  # reverse words if necessary
   if offset_dir == "prev":
     data["words_list"] = data["words_list"][::-1]
 
   # if there are words
   if data["words_list"]:
      
-    # add other pagination data
+    # add order_dir specific pagination data
     if order_dir == "ASC":
-        data["first_rec"] = query_db(sql[1][0], one = True)[offset_field]
-        data["last_rec"]  = query_db(sql[2][0], one = True)[offset_field]
+        data["first_rec"] = query_db(sql[2][0], sql[2][1], one = True)[offset_field]
+        data["last_rec"]  = query_db(sql[3][0], sql[3][1], one = True)[offset_field]
     else:
-        data["first_rec"] = query_db(sql[2][0], one = True)[offset_field]
-        data["last_rec"]  = query_db(sql[1][0], one = True)[offset_field]
+        data["first_rec"] = query_db(sql[3][0], sql[3][1], one = True)[offset_field]
+        data["last_rec"]  = query_db(sql[2][0], sql[2][1], one = True)[offset_field]
 
+    # add other pagination data
     data["prev_rec"]      = data["words_list"][0][offset_field]
     data["next_rec"]      = data["words_list"][-1][offset_field]
     data["is_first_page"] = data["first_rec"] == data["prev_rec"]
     data["is_last_page"]  = data["last_rec"] == data["next_rec"]
-    data["prev_url"]      = url_for("main.list", offset_field = offset_field, offset_value = data["prev_rec"], offset_dir = "prev", order_dir = order_dir)
-    data["next_url"]      = url_for("main.list", offset_field = offset_field, offset_value = data['next_rec'], offset_dir = "next", order_dir = order_dir)
+    data["offset_field"]  = offset_field
+    data["offset_value"]  = offset_value
+    data["offset_dir"]    = offset_dir
+    data["filter_field"]  = filter_field
+    data["filter_value"]  = filter_value
+    data["order_dir"]     = order_dir
   
   # render view
   return render_template("main/index.html", data = data)
